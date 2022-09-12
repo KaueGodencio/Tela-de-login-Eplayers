099e7de538cffc09a317f915e08f47a2094c4192
kaue godencio <you@example.com>
2022-09-12 09:55:21 -0300

    Validaçoes com JavaScript

---
+++ js/login.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+    $('#sign-in').click(function () { 
+        validarLogin();
+    }); 
+
+ });
+
+ function validarLogin(){
+    let usuario = document.getElementById('usuario').value;
+    let senha = document.getElementById('senha').value;
+    
+    if(senha.trim () =='' || senha.trim() =='' ){
+        alert('Por favor preencher todos os campos.')
+    }else{
+        alert('Você preencheu corretamente.'); }
+
+    }
