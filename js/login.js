63574a0ca880b0cee06785ffefa6d6492ce4eb33
kaue godencio <you@example.com>
2022-09-12 17:56:13 -0300

    adicionei bootstrap e funcionalidades javaScript

--- js/login.js
+++ js/login.js
@@ -12,6 +12,8 @@ $(document).ready(function (){
     if(usuario.trim () =='' || senha.trim() =='' ){
         alert('Por favor preencher todos os campos.')
     }else{
-        alert('Você preencheu corretamente.'); }
+        alert('Você não possui cadastro, click em criar conta.'); }
 
     }
+
+
